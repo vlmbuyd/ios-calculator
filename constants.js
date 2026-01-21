ade6a06ca26983802fdc9b5c7242994fd1f4e801
김예안 <kjan0929@naver.com>
2026-01-21 17:43:11 +0900

    refactor(constant): 연산자 우선순위에 따라 상수 객체화하여 분리

--- constants.js
+++ constants.js
@@ -1,8 +1,17 @@
 // 화면 초기값
 export const INITIAL_DISPLAY_VALUE = '0';
 
-// 연산자 목록
-export const OPERATROS = ['+', '−', '×', '÷'];
+// 연산자 우선순위
+export const OPERATROS_PRIORITES = Object.freeze({
+  HIGH: ['×', '÷'],
+  LOW: ['+', '−'],
+});
+
+// 전체 연산자 목록
+export const OPERATROS = [
+  ...OPERATROS_PRIORITES.HIGH,
+  ...OPERATROS_PRIORITES.LOW,
+];
 
 // 에러 메시지
 export const ERROR_MESSAGE = 'NaN';
