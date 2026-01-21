7e4f7a7491c964e97ce985b1ff4934e4ca154b68
김예안 <kjan0929@naver.com>
2026-01-21 17:43:50 +0900

    feat(calculator): 최종 계산 로직 구현

--- model/Calculator.js
+++ model/Calculator.js
@@ -1,4 +1,8 @@
-import { INITIAL_DISPLAY_VALUE, OPERATROS } from '../constants.js';
+import {
+  INITIAL_DISPLAY_VALUE,
+  OPERATROS,
+  OPERATROS_PRIORITES,
+} from '../constants.js';
 
 export default class Calculator {
   /**
@@ -63,8 +67,68 @@ export default class Calculator {
     this.updateDisplay();
   }
 
-  compute() {}
+  /**
+   * 단일 연산 수행
+   * @param {Array<'+' | '−' | '×' | '÷'>} operator - 연산자
+   * @param {number} prev - 이전 피연산자
+   * @param {number} next - 다음 피연산자
+   * @returns {number} - 연산 결과
+   */
+  calculateOperation(operator, prev, next) {
+    switch (operator) {
+      case '×':
+        return prev * next;
+      case '÷':
+        return prev / next;
+      case '+':
+        return prev + next;
+      case '−':
+        return prev - next;
+    }
+  }
 
+  /**
+   * 연산자 우선순위에 따른 계산 수행
+   * @param {Array<string>} expression - 계산식 배열
+   * @param {Array<'+' | '−' | '×' | '÷'>} targetOperators - 타겟 연산자 배열
+   */
+  calculateByPriority(expression, targetOperators) {
+    for (let i = 0; i < expression.length; i++) {
+      const currValue = expression[i];
+
+      if (targetOperators.includes(currValue)) {
+        // 연산자 양옆의 피연산자
+        const prev = Number(expression[i - 1]);
+        const next = Number(expression[i + 1]);
+
+        // 단일 연산 수행
+        const result = this.calculateOperation(currValue, prev, next);
+
+        // 배열 업데이트: prev, operator, next -> result로 교체
+        expression.splice(i - 1, 3, result);
+        i--; // 배열 길이 변경에 따른 인덱스 조정
+      }
+    }
+  }
+
+  /**
+   * 최종 결과 계산
+   */
+  calculateResult() {
+    const expression = [...this.history, this.currOperand];
+
+    // [연산자 우선순위] 곱셈/나눗셈 -> 덧셈/뺄셈 순으로 순회하여 계산
+    this.calculateByPriority(expression, OPERATROS_PRIORITES.HIGH);
+    this.calculateByPriority(expression, OPERATROS_PRIORITES.LOW);
+
+    // 결과 화면에 표시
+    const result = expression[0];
+    this.displayEl.innerText = result;
+  }
+
+  /**
+   * 화면 업데이트
+   */
   updateDisplay() {
     const historyStr = this.history.join('');
     this.displayEl.innerText = `${historyStr}${this.currOperand}`;
